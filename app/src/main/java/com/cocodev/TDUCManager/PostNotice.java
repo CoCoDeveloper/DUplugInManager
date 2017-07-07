52f0d14797ba95bc1efba79960e2801253db8680
mr-darksoul <akonvs.manav@gmail.com>
2017-07-07 21:39:01 +0530

    Update PostNotice.java
    Added: menu , resolved conflict of empty field

--- app/src/main/java/com/cocodev/TDUCManager/PostNotice.java
+++ app/src/main/java/com/cocodev/TDUCManager/PostNotice.java
@@ -1,30 +1,51 @@
 package com.cocodev.TDUCManager;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.cocodev.TDUCManager.Utility.Notice;
+import com.cocodev.TDUCManager.Utility.User;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class PostNotice extends AppCompatActivity {
-
+public class PostNotices extends AppCompatActivity {
+    static int CHECKER = 0;
     DatabaseReference mNoticeRef;
     EditText mDesc,mDepartment,mDeadline;
     Button mSubmit;
-
     Notice notice;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    public static User currentUser;
+    String description;
+    String department;
+    String deadline;
+    String time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notices);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Upload Notices");
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#009688")));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
         mNoticeRef = FirebaseDatabase.getInstance().getReference().child("notice");
         mDesc = (EditText)findViewById(R.id.editText_notice_desc);
         mDepartment = (EditText)findViewById(R.id.editText_notice_department);
@@ -39,17 +60,37 @@ public class PostNotice extends AppCompatActivity {
     }
     private void bindData()
     {
-        String description = mDesc.getText().toString();
-        String department = mDepartment.getText().toString();
-        String deadline = mDeadline.getText().toString();
-        String time = getCurrentTime();
-        notice = new Notice(department,time,deadline,description);
-        mNoticeRef.push().setValue(notice).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Toast.makeText(PostNotice.this,"Notice Uploaded!",Toast.LENGTH_SHORT).show();
-            }
-        });
+        description = mDesc.getText().toString();
+        department = mDepartment.getText().toString();
+        deadline = mDeadline.getText().toString();
+        time = getCurrentTime();
+        if(checkFields()) {
+            notice = new Notice(department, time, deadline, description);
+            mNoticeRef.push().setValue(notice).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(Notices.this, "Notice Uploaded!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
+    private boolean checkFields()
+    {
+        if(TextUtils.isEmpty(description)) {
+            mDesc.setError("Field must not be empty");
+            return false;
+        }
+        if(TextUtils.isEmpty(department)) {
+            mDepartment.setError("Field must not be empty");
+            return false;
+        }
+        if(TextUtils.isEmpty(deadline)) {
+            mDeadline.setError("Field must not be empty");
+            return false;
+        }
+        return true;
+
     }
     private static String getCurrentTime()
     {
@@ -62,5 +103,31 @@ public class PostNotice extends AppCompatActivity {
         Intent i = new Intent(this,MainActivity.class);
         startActivity(i);
         finish();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_logout:
+                currentUser=null;
+                mFirebaseUser=null;
+                mFirebaseAuth.signOut();
+                Intent intent = new Intent(this,Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                finish();
+                startActivity(intent);
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
