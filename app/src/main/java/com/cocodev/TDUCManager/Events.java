dda7ed2531c8ec944750bb17f375fbbdf466ab10
mr-darksoul <akonvs.manav@gmail.com>
2017-07-07 21:35:56 +0530

    Update Events.java
    Added: Upload image to firebase resolved conflicts of empty fields.

--- app/src/main/java/com/cocodev/TDUCManager/Events.java
+++ app/src/main/java/com/cocodev/TDUCManager/Events.java
@@ -1,66 +1,219 @@
 package com.cocodev.TDUCManager;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.cocodev.TDUCManager.Utility.Event;
+import com.cocodev.TDUCManager.Utility.User;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class Events extends AppCompatActivity {
+    ImageView imgView;
+    int PICK_IMAGE_REQUEST = 111;
+    Uri filePath;
+    ProgressDialog progressDialog;
     DatabaseReference mEventRef;
-    EditText mTitle,mDesc,mVenue,mImageUrl;
-    Button mSubmit;
+    EditText mTitle, mDesc, mVenue, mImageUrl;
+    Button mSubmit, mImagePicker;
     Event event;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    public static User currentUser;
+    String description;
+    String time;
+    String title;
+    String image;
+    String venue;
+    FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+    StorageReference storageReference = firebaseStorage.getReference();
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Upload Events");
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#009688")));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Uploading...");
         mEventRef = FirebaseDatabase.getInstance().getReference().child("events");
-        mTitle = (EditText)findViewById(R.id.editText_event_title);
-        mDesc = (EditText)findViewById(R.id.editText_event_desc);
-        mVenue = (EditText)findViewById(R.id.editText_event_venue);
-        mImageUrl = (EditText)findViewById(R.id.editText_event_image);
+        mTitle = (EditText) findViewById(R.id.editText_event_title);
+        mDesc = (EditText) findViewById(R.id.editText_event_desc);
+        mVenue = (EditText) findViewById(R.id.editText_event_venue);
+        mImageUrl = (EditText) findViewById(R.id.editText_event_image);
         mSubmit = (Button) findViewById(R.id.button_event_submit);
+        imgView = (ImageView) findViewById(R.id.image_view_show);
+        mImagePicker = (Button) findViewById(R.id.button_image_picker_event);
         mSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bindData();
+                uploadImage();
+            }
+        });
+        mImagePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_PICK);
+                startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE_REQUEST);
             }
         });
     }
-    private void bindData()
-    {
-        String description = mDesc.getText().toString();
-        String time = getCurrentTime();
-        String title = mTitle.getText().toString();
-        String image = mImageUrl.getText().toString();
-        String venue = mVenue.getText().toString();
-        event = new Event(venue,time,description,image,title);
-        mEventRef.push().setValue(event).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Toast.makeText(Events.this,"Event Uploaded!", Toast.LENGTH_LONG).show();
+
+    private void uploadImage() {
+        if (filePath != null) {
+            progressDialog.show();
+
+            StorageReference childRef = storageReference.child("Events").child(filePath.getLastPathSegment());
+            //uploading the image
+            UploadTask uploadTask = childRef.putFile(filePath);
+
+
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progressDialog.dismiss();
+                    Uri imageurl = taskSnapshot.getDownloadUrl();
+                    image = imageurl.toString();
+                    Toast.makeText(Events.this, "Upload successful", Toast.LENGTH_SHORT).show();
+                    bindData();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(getApplicationContext(), "Upload Failed -> " + e, Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "Select an image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+
+            try {
+                //getting image from gallery
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+
+                //Setting image to ImageView
+                imgView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
+        }
+    }
+
+    private void bindData() {
+        description = mDesc.getText().toString();
+        time = getCurrentTime();
+        title = mTitle.getText().toString();
+        venue = mVenue.getText().toString();
+        mImageUrl.setText(image);
+
+        if (checkFields()) {
+            event = new Event(venue, time, description, image, title);
+            mEventRef.push().setValue(event).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(Events.this, "Event Uploaded!", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
 
     }
+
+    private boolean checkFields() {
+        if (TextUtils.isEmpty(description)) {
+            mDesc.setError("Field must not be empty");
+            return false;
+        }
+        if (TextUtils.isEmpty(title)) {
+            mTitle.setError("Field must not be empty");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(venue)) {
+            mVenue.setError("Field must not be empty");
+            return false;
+        }
+        return true;
+    }
+
     private static String getCurrentTime() {
         Long time = System.currentTimeMillis();
         String ts = time.toString();
-        return  ts;
+        return ts;
     }
+
     @Override
     public void onBackPressed() {
-        Intent i = new Intent(this,MainActivity.class);
+        Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_logout:
+                currentUser = null;
+                mFirebaseUser = null;
+                mFirebaseAuth.signOut();
+                Intent intent = new Intent(this, Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                finish();
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
